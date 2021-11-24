d0a5c688d8019ae23c64fd90ccd383fe58c69aff
MJGR <mjgriley@gmail.com>
2021-11-24 10:19:10 -0500

    template files added

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
--- server/models/User.js
+++ server/models/User.js
@@ -25,7 +25,7 @@ const userSchema = new Schema({
     required: true,
   },
   dob: {
-    type: Date,
+    type: DateTime,
     required: true,
   },
   password: {
