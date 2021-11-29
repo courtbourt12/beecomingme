256e95fe783e730524388644f9c38f67ee293834
Courtney Long <courtneylong877@gmail.com>
2021-11-29 10:17:35 -0500

    Added modals to sign up and login

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,45 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 export default function SignUp() {
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div>
-      <form action="submit">
+      <Button variant="primary" size="lg" active onClick = {handleShow}>
         Sign Up
-        <br />
-        <input name="email" id="email" placeholder="Email" type="email" />
-        <br />
-        <input
-          name="firstName"
-          id="firstName"
-          placeholder="First Name"
-          type="text"
-        />
-        <br />
-        <input
-          name="lastName"
-          id="lastName"
-          placeholder="Last Name"
-          type="text"
-        />
-        <br />
-        <input name="dob" id="dob" type="date" />
-        <br />
-        <input
-          name="username"
-          id="username"
-          placeholder="Username"
-          type="text"
-        />
-        <br />
-        <input
-          name="password"
-          id="password"
-          placeholder="Password"
-          type="password"
-        />
-        <br />
-        <input type="submit" value="Sign Up" />
-      </form>
+      </Button>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        centered
+        backdrop="static"
+        keyboard={false}
+      >
+        <form action="submit">
+          Sign Up
+          <br />
+          <input name="email" id="email" placeholder="Email" type="email" />
+          <br />
+          <input
+            name="firstName"
+            id="firstName"
+            placeholder="First Name"
+            type="text"
+          />
+          <br />
+          <input
+            name="lastName"
+            id="lastName"
+            placeholder="Last Name"
+            type="text"
+          />
+          <br />
+          <input name="dob" id="dob" type="date" />
+          <br />
+          <input
+            name="username"
+            id="username"
+            placeholder="Username"
+            type="text"
+          />
+          <br />
+          <input
+            name="password"
+            id="password"
+            placeholder="Password"
+            type="password"
+          />
+          <br />
+          <div className="buttonDiv">
+            <Button variant="outline-dark" onClick={handleClose}>
+                Close
+            </Button>
+            <Button
+              variant="outline-dark"
+              onClick={handleClose}
+            >
+              Sign Up
+            </Button>
+          </div>
+        </form>
+        </Modal>
     </div>
   );
 }
+
