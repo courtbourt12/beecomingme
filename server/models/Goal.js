58b0850143b5d640de54e5d46aa11dc26401af74
JoeyGitsIt <josephperri3@gmail.com>
2021-11-30 15:18:27 -0500

    the whole backend is working including friendgoals and removes

--- server/models/Goal.js
+++ server/models/Goal.js
@@ -1,28 +1,27 @@
 const { Schema } = require("mongoose");
 const stepSchema = require("./Step.js");
 
-const goalSchema = new Schema(
-  {
-    title: {
-      type: String,
-      trim: true,
-      required: "Enter a Title",
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    steps: [stepSchema],
-    friends: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    encouragement: {
-      type: Number,
-    },
+const goalSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: "Enter a Title",
   },
-);
+  description: {
+    type: String,
+    required: true,
+  },
+  steps: [stepSchema],
+  friends: [
+    {
+      username: {
+        type: String,
+      },
+    },
+  ],
+  encouragement: {
+    type: Number,
+  },
+});
 
 module.exports = goalSchema;
