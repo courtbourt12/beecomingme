8db9314ed8348e08fc21138a174789d8c92e3941
Courtney Long <courtneylong877@gmail.com>
2021-11-30 22:00:14 -0500

    styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,79 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import SignUpForm from "./Register";
-import LoginForm from "../pages/Login";
-
-import Auth from "../utils/auth";
-
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import "../scss/Navbar.scss";
+ 
 const AppNavbar = () => {
-  // set modal display state
-  const [showModal, setShowModal] = useState(false);
-
+ 
+ const logout = () => {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('user');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container fluid>
-          <Navbar.Brand as={Link} to="/">
-            Google Books Search
+      <Navbar className = "theNavbar" bg="dark" expand="lg">
+        <Container>
+          <Navbar.Brand className="text-white" as={Link} to="/">
+            Beecoming Me
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar" />
-          <Navbar.Collapse id="navbar">
-            <Nav className="ml-auto">
-              <Nav.Link as={Link} to="/">
-                Search For Books
+          <Navbar.Toggle className="navbar-dark navbar-toggler-icon-light" aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link className="text-white" as={Link} to="/myprofile">
+                My Profile
               </Nav.Link>
-              {/* if user is logged in show saved books and logout */}
-              {Auth.loggedIn() ? (
-                <>
-                  <Nav.Link as={Link} to="/saved">
-                    See Your Books
-                  </Nav.Link>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>
-                  Login/Sign Up
-                </Nav.Link>
-              )}
+              <Nav.Link className="text-white" as={Link} to="/friendgoals">
+                Friends' Goals
+              </Nav.Link>
+              <Nav.Link className="text-white" onClick={logout} to="/">
+                Logout
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {/* set modal data up */}
-      <Modal
-        size="lg"
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby="signup-modal"
-      >
-        {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey="login">
-          <Modal.Header closeButton>
-            <Modal.Title id="signup-modal">
-              <Nav variant="pills">
-                <Nav.Item>
-                  <Nav.Link eventKey="login">Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey="login">
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey="signup">
-                <SignUpForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
     </>
   );
 };
-
+ 
 export default AppNavbar;
+
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,39 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
-import Auth from "../utils/auth";
-
-export default function Login() {
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+ 
+export default function LoginForm() {
+  // Handling modal open and close functions.
+ 
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+ 
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
-
-
-  const [login] = useMutation(LOGIN_USER);
-
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+ 
+  const [login, { error }] = useMutation(LOGIN_USER);
+ 
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
+ 
+ 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
-
+ 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
     const form = event.currentTarget;
+ 
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
     }
-console.log("submit success " + userFormData.email + " " + userFormData.password)
     try {
       const { data } = await login({
         variables: { ...userFormData },
       });
-
-      console.log(data);
-      Auth.login(data.login.token);
+      localStorage.setItem("user", JSON.stringify(data.login));
+      window.location.assign('/myprofile');
+      handleClose();
+      // Auth.login(data.login.token);
     } catch (e) {
       console.error(e);
     }
-
     // clear form values
     setUserFormData({
       email: "",
@@ -42,29 +60,70 @@ console.log("submit success " + userFormData.email + " " + userFormData.password
   };
   return (
     <div>
-      <form className="loginForm" onSubmit={handleFormSubmit}>
-        Sign In
-        <br />
-        <input
-          onChange={handleInputChange}
-          name="email"
-          id="email"
-          placeholder="Email"
-          type="email"
-          value={userFormData.email}
-        />
-        <br />
-        <input
-          onChange={handleInputChange}
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-          value={userFormData.password}
-        />
-        <br />
-        <input type="submit" />
-      </form>
+      <Button variant="dark" size="lg" active onClick = {handleShow}>
+        Login
+      </Button>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        centered
+        backdrop="static"
+        keyboard={false}
+      >
+        <Form className= "loginForm" noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Alert
+            dismissible
+            onClose={() => setShowAlert(false)}
+            show={showAlert}
+            variant="danger"
+          >
+            Something went wrong with your login credentials!
+          </Alert>
+          <Form.Group>
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Control
+              type="input"
+              placeholder="Your email"
+              name="email"
+              onChange={handleInputChange}
+              value={userFormData.email}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Email is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+ 
+          <Form.Group>
+            <Form.Label htmlFor="password">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Your password"
+              name="password"
+              onChange={handleInputChange}
+              value={userFormData.password}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Password is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button
+            variant="outline-dark"
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+          <Button
+            disabled={!(userFormData.email && userFormData.password)}
+            type="submit"
+            variant="outline-dark"
+          >
+            Submit
+          </Button>
+        </Form>
+      </Modal>
     </div>
   );
 };
