609bb9680a22b085768b32de2f3efaa2bdc8959f
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-11-30 22:22:52 -0500

    changed httpLink for external deployment

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
