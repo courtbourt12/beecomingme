1adc05f14925274a9d098c5678fc3c0386ad2881
Courtney Long <courtneylong877@gmail.com>
2021-11-30 19:39:07 -0500

    Step form works

--- client/src/components/AddStepForm.js
+++ client/src/components/AddStepForm.js
@@ -1,40 +1,73 @@
 import { React, useState } from "react";
 import Button from "react-bootstrap/Button";
+import { useMutation } from "@apollo/client";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 import "../scss/MyGoalDisplay.scss";
-
-const newStep = [];
+import { ADD_STEP } from "../utils/mutations";
 
 export default function AddStepForm() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [ inputTitle, setInputTitle] = useState('');
-  const [ inputDescription, setInputDescription] = useState('');
+  const [stepData, setStepData] = useState({
+    title: "",
+    description: "",
+    due:"",
+    status:""
+  });
 
-  const handleSubmit = (event) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const userID = user._id
+
+  let url = window.location.href;
+  let goalID = url.substring(url.length - 24, url.length);
+   
+  const [addStep] = useMutation(ADD_STEP);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setStepData({ ...stepData, [name]: value});
+  };
+
+  const AddStep = async (event) => {
     event.preventDefault();
-    const newT = inputTitle
-    const newD = inputDescription
-    newStep.push({title: newT,
-                  description: newD})
-    console.log(inputTitle, inputDescription)
-    console.log(newStep)
-  
-    setInputTitle('');
-    setInputDescription('');
-    handleClose();
-  };
-  
-  
-  const handleTitleChange = (e) => {
-    setInputTitle(e.target.value);
-  };
-  
-  const handleDescriptionChange = (e) => {
-    setInputDescription(e.target.value)
-  };
+    try {
+
+      const asAString = stepData.status;
+      const asAnInteger = parseInt(asAString, 10)
+      console.log(typeof asAnInteger)
+
+      const toMutate = {
+        title: stepData.title,
+        description: stepData.description,
+        due: stepData.due,
+        status: asAnInteger,
+        user: userID,
+        goal: goalID
+      }
+      console.log(toMutate)
+
+      const { data } = await addStep({
+        variables: { inputStep: {...toMutate} },
+        });
+        console.log("the data", data)
+
+      handleClose();
+
+    } catch (error) {
+      console.error(error);
+    }
+
+    // clear form values
+    setStepData({
+      title: "",
+      description: "",
+      due:"",
+      status:""
+    });
+  }
 
   return (
     <div>
@@ -59,46 +92,81 @@ export default function AddStepForm() {
         backdrop="static"
         keyboard={false}
       >
-        <form className="addStepForm">
-          <input
-            name="addStepTitle"
+        <Form className="addStepForm" onSubmit={AddStep}>
+          <Form.Group>
+          <Form.Label htmlFor="title">Step Title</Form.Label>
+          <Form.Control
+            type="input"
+            name="title"
             id="addStepTitle"
-            value= {inputTitle}
-            placeholder="Title*"
-            type="text"
-            onChange= {handleTitleChange}
+            value={stepData.title}
+            onChange={handleInputChange}
+            required
           />
-          <input
-            name="addStepDescription"
+        <Form.Control.Feedback type="invalid">
+            Step title is required!
+        </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="description">Step Description</Form.Label>
+          <Form.Control
+            name="description"
             id="addStepDescription"
-            value= {inputDescription}
-            placeholder="Description*"
-            type="text"
-            onChange= {handleDescriptionChange}
+            value={stepData.description}
+            type="input"
+            onChange={handleInputChange}
+            required
           />
-          <br />
-          <label name="dueDate">Due Date</label>
-          <input name="dueDate" id="dueDate" type="date" />
-          <br />
-          <select>
+          <Form.Control.Feedback type="invalid">
+            Step description is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="due">Due Date</Form.Label>
+          <Form.Control
+            type="date"
+            name="due"
+            id="addDue"
+            value={stepData.due}
+            onChange={handleInputChange}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Due date is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="due">Step Status</Form.Label>
+          <select class="form-select" aria-label="Default select example"
+           name="status" onChange={handleInputChange}>
+            <option selected>Open this select menu</option>
             <option value="1">Not Started</option>
             <option value="2">In Progress</option>
             <option value="3">Complete</option>
           </select>
-          <br />
-          <div className="buttonDiv">
+          {/* <Form.Control
+            type="select"
+            name="status"
+            id="addStatus"
+            value={stepData.status}
+            onChange={handleInputChange}
+            required
+          /> */}
+          <Form.Control.Feedback type="invalid">
+            Status is required!
+          </Form.Control.Feedback>
+        </Form.Group>
             <Button variant="outline-dark" onClick={handleClose}>
               Close
             </Button>
             <Button
               className="addStep"
               variant="outline-dark"
-              onClick={handleSubmit}
+              type="submit"
             >
               Add Step
             </Button>
-          </div>
-        </form>
+        </Form>
       </Modal>
     </div>
   );
